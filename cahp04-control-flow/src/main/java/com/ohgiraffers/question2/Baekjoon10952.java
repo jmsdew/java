ec0592b8effcc94d76b064882ae9dc32f724547b
jmsdew <jmsdew@naver.com>
2023-11-10 08:40:42 +0900

    list

---
+++ cahp04-control-flow/src/main/java/com/ohgiraffers/question2/Baekjoon10952.java
@@ -0,0 +1,31 @@
+package main.java.com.ohgiraffers.question2;
+
+import java.util.Scanner;
+
+public class Baekjoon10952 {
+    public static void main(String[] args){
+
+        Scanner scr = new Scanner(System.in);
+
+
+        while(true){
+            String a =scr.nextLine();
+            String[] b = a.split(" ");
+            int[] c = new int[2];
+            for (int i = 0; i < 2; i++) {
+                c[i] = Integer.parseInt(b[i]);
+            }
+            if(c[0] == c[1] && c[0] == 0){
+                return;
+            }else {
+                System.out.println(c[0] + c[1]);
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ cahp04-control-flow/src/main/java/com/ohgiraffers/question2/Baekjoon2908.java
@@ -0,0 +1,27 @@
+package main.java.com.ohgiraffers.question2;
+
+import java.util.Scanner;
+
+public class Baekjoon2908 {
+    public static void main(String[] args){
+
+        Scanner scr = new Scanner(System.in);
+        String a = scr.nextLine();
+        String[] b = a.split(" ");
+
+        String c ="";
+        String d ="";
+        for (int i = b[0].length()-1; i >=0 ; i--) {
+            c = c+b[0].charAt(i);
+        }
+        for (int i = b[1].length()-1; i >= 0; i--) {
+            d = d+b[1].charAt(i);
+        }
+        if(Integer.parseInt(c) > Integer.parseInt(d)){
+            System.out.println(c);
+        }else {
+            System.out.println(d);
+        }
+
+    }
+}
