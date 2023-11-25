4cee72436b3cdb2d74350ae78d9b42e1ad7eea35
jmsdew <jmsdew@naver.com>
2023-11-25 19:31:43 +0900

    list

---
+++ cahp04-control-flow/src/main/java/com/ohgiraffers/question2/Baekjoon11718.java
@@ -0,0 +1,14 @@
+package main.java.com.ohgiraffers.question2;
+
+import java.util.Scanner;
+
+public class Baekjoon11718 {
+    public static void main(String[] args){
+        Scanner scr = new Scanner(System.in);
+
+        while(scr.hasNext()){
+            String a = scr.nextLine();
+            System.out.println(a);
+        }
+    }
+}
